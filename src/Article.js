f39c30f2fe830439636cdb8406b08f74c08bcc29
Hoyong Lee <igotoweb@gmail.com>
2021-12-29 01:24:35 +0900

    feat: 수정 삭제용 툴바 추가

--- src/Article.js
+++ src/Article.js
@@ -1,13 +1,17 @@
 import Avatar from "./Avatar";
+import Toolbar from "./Toolbar"
 
 export default function Article({ title, children, author, lastUpdated }) {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-screen-lg m-6">
+    <div className="group max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-screen-lg m-6">
       <div className="md:flex">
         <div className="md:shrink-0">
           <img className="h-48 w-full object-cover md:h-full md:w-48" src="https://urclass.codestates.com/static/media/cover.23e06f01.jpg" alt="Man looking at item at a store" />
         </div>
         <div className="p-8">
+          <div className="transition-opacity md:opacity-0 group-hover:opacity-100">
+            <Toolbar />
+          </div>
           <div className="block mt-1 text-xl leading-tight font-medium text-black mb-2">{title}</div>
           <Avatar name={author} picture={'https://avatars.githubusercontent.com/u/702622?v=4'} />
           <div className="mt-2 text-sm">{lastUpdated}</div>
